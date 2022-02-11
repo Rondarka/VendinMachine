b3de1e78f7606668695a657abd2a0323e53d79e7
Rondara Senbonzakura <laymed@mail.ru>
2022-02-11 13:23:04 +0300

    app

---
+++ VendingMachine/VendingMachines/pages/Otchet.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VendingMachines.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Otchet.xaml
+    /// </summary>
+    public partial class Otchet : Page
+    {
+        public Otchet()
+        {
+            InitializeComponent();
+            VendingEntities database = new VendingEntities();
+            reportGrid.ItemsSource = database.Report.ToList();
+
+            actuallyData.Content = DateTime.Today;
+        }
+
+        private void reportGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void savePdf_Click(object sender, RoutedEventArgs e)
+        {
+     
+        }
+    }
+}
